fea39d15b003f5e1a82086c1eb565b160a8a4cbe
isaacbarcroft <i_barcroft@yahoo.com>
2021-12-08 09:02:35 -0500

    JS

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 const checkboxes = document.querySelectorAll('.inbox input[type = "checkbox"]');
+
+let lastChecked;
+
+function handleChecked(e) {
+    // console.log(e);
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(box => {
+            if (box === this || box === lastChecked) {
+                inBetween = !inBetween
+            }
+            if (inBetween) {
+                box.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(box => box.addEventListener('click', handleChecked))
